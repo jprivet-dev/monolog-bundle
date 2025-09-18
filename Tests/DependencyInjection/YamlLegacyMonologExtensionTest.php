b7ed0a11be2021c8cece6f7de5357e0df3c40b8a
jprivet-dev <jprivet.dev@gmail.com>
2025-09-18 09:46:47 +0200

    feat(YamlLegacyMonologExtensionTest): create base of test

---
+++ Tests/DependencyInjection/YamlLegacyMonologExtensionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DependencyInjection;
+
+use Symfony\Bundle\MonologBundle\Tests\DependencyInjection\FixtureMonologExtensionTestCase;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class YamlLegacyMonologExtensionTest extends FixtureMonologExtensionTestCase
+{
+    protected function loadFixture(ContainerBuilder $container, $fixture)
+    {
+        $container->setDefinition('mailer', new Definition('Swiftmailer'));
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/Fixtures/yml/legacy'));
+        $loader->load($fixture.'.yml');
+    }
+}
